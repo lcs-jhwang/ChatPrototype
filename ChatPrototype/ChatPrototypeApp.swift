6f7b4255151e0e2d5280256cdb734b5aa1ddd25b
Julien Hwang <jhwang@lcs.on.ca>
2024-12-03 08:42:09 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Julien Hwang on 3/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
